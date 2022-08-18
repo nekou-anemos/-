e11f5c807e4a4fc5f92e9372eedb08b76acee1b7
quote-meta <nullpo.loveletter.vbs@gmail.com>
2022-08-18 15:06:16 +0900

    extract zip file

---
+++ src/main/java/com/ANEMOS/fantasyplayersmod/main/Fantasyplayersmod.java
@@ -0,0 +1,13 @@
+package com.ANEMOS.fantasyplayersmod.main;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("fantasyplayersmod")
+public class Fantasyplayersmod {
+
+    public static final String MOD_ID = "fantasyplayersmod";
+
+    public static final CreativeModeTab FANTASYPLAYERSMOD_TAB = new FantasyplayersmodTab();
+
+}
